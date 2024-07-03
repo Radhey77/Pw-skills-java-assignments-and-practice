23de70b5a60c8c689a07a23c4b85ed7a239614b2
Your Name <radheyanpatil@gmail.com>
2024-07-03 14:33:03 +0530

    average

---
+++ basic fundamentals/Average.java
@@ -0,0 +1,9 @@
+class Average{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 24;
+        int c = 42;
+        int average= (a+b+c)/3;
+        System.out.println("Average="+ average);
+    }
+}
